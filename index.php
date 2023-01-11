b17886cac90bd121429b990326795bbb7ec393da
Jessica <jesscara.993@gmail.com>
2023-01-11 17:00:06 +0100

    created function for random password

--- index.php
+++ index.php
@@ -8,20 +8,34 @@
   <title>Password Generator</title>
 
   <?php
-  $getPass = $_GET["password"] ?? '';
+  // prendo lunghezza password decisa da utente
+  $passLength = $_GET["password"] ?? '';
+
+  // funzione per generare password random in base a lunghezza decisa da utente
+  function getRandomPassword($passLength)
+  {
+    // parametri divisi in categorie per eventuale milestone4
+    $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+    $numbers = '1234567890';
+    $symbols = '!?@#$&*';
+    $chars = $letters . $numbers . $symbols;
+    // ritorna stringa della lunghezza data da utente randomizzata
+    return substr(str_shuffle($chars), 0, $passLength);
+  }
   ?>
 </head>
 
 <body>
   <h1>Password generator</h1>
-  <form method="post">
+  <form>
     <label for="password"> Lunghezza password: </label>
     <input type="number" name="password" required>
     <input type="submit" value="Genera">
 
     <h3>
       <?php
-      echo "Password generata:" . " " . " funzione ancora da creare"
+      // var_dump($passLength);
+      echo "Password generata:" . " " . getRandomPassword($passLength)
         ?>
     </h3>
   </form>
